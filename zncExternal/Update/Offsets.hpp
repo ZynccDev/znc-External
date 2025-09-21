6b44e7f96437b39b7903b536d55a2f0a542dea2a
ZynccDev <ai.19.ai.suno@gmail.com>
2025-09-21 10:28:06 +1000

    Create Offsets.hpp

---
+++ zncExternal/Update/Offsets.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#include <Windows.h>
+
+namespace offsets {
+	// Instance
+    inline uintptr_t This = 0x8;
+    inline uintptr_t name = 0x88;
+    inline uintptr_t parent = 0x58;
+    inline uintptr_t children = 0x68;
+    inline uintptr_t children_end = 0x8;
+    inline uintptr_t Gravity = 0x1AC;
+    inline uintptr_t GravityInfo = 0x3d8;
+
+    inline uintptr_t cframe = 0x130;
+
+    // Player
+    inline uintptr_t localplayer = 0x128;
+    inline uintptr_t humanoidstate = 0x870;
+    inline uintptr_t walkspeed = 0x1DC;
+	inline uintptr_t walkspeedcheck = 0x3B8; // no suspicious client kicks if we use this soo W
+    inline uintptr_t jumppower = 0x1B8;
+    inline uintptr_t health = 0x19C;
+
+	inline uintptr_t cancollide = 0x2E5;
+    inline uintptr_t primitive = 0x178;
+	// DataModel
+    inline uintptr_t fakedatamodel = 0x70A86B8;
+    inline uintptr_t fakedm_to_dm = 0x1C0;
+    // ClassName
+    inline uintptr_t classdescriptor = 0x18;
+    inline uintptr_t classdescriptortoclassname = 0x18;
+}
